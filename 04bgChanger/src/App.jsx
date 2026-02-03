f5328e5576e0fc811d090ebb059ff276df485b43
Vivek18 <viveksaha096@gmail.com>
2026-02-03 16:44:11 +0530

    Background Color Project Completed!

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -6,9 +6,45 @@ import './index.css'
 
 
 function App() {
-  const [color, setColor] = useState("blue")
+  
+  let [color, setColor] = useState("blue")
+  // Red Background color
+  const addRed = () => {
+    color = "red";
+    setColor(color);
+  }
+  // Green Background Color
+  const addGreen = () => {
+    color = 'green';
+    setColor(color);
+  } 
+  // Purple Background Color
+  const addPurple = () => {
+    color = 'purple';
+    setColor(color);
+  }
+  // Yellow Background Color
+  const addYellow = () => {
+    color = 'yellow';
+    setColor(color);
+  }
   return (
    <div className='w-full h-screen' style={{backgroundColor:color}}> 
+
+   <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-5 gap-10 bg-amber-300 rounded-3xl py-3 '>
+      <button onClick={addRed}>
+        Red
+      </button>
+      <button onClick={addGreen}>
+        Green
+      </button>
+      <button onClick={addPurple}>
+        Purple
+      </button>
+      <button onClick={addYellow}>
+        Yellow
+      </button>
+   </div>
 
    </div>
   )
