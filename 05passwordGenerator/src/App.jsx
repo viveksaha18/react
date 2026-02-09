f6ff8cffb19f41da8bbde7457e1d64180edd6187
Vivek18 <viveksaha096@gmail.com>
2026-02-09 19:44:37 +0530

    Working on

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-4xl text-center text-white'>Password Generator</h1>
+    </>
+  )
+}
+
+export default App
