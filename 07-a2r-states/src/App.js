e8efbac6f4f9409f56e6e79f2d7e0b934f089d5a
Vivek18 <viveksaha096@gmail.com>
2026-07-25 22:05:21 +0530

    states in react

---
+++ 07-a2r-states/src/App.js
@@ -0,0 +1,48 @@
+
+import EmployeeData from './EmployeeData';
+import {useEffect, useState} from 'react';
+function App() {
+  const[data, setData] = useState([]);
+  const[id, setId] = useState(0);
+  const [name, setName] = useState('');
+  const [position, setPosition] = useState('');
+  const [department, setDepartment] = useState('');
+  const [salary, setSalary] = useState(0);
+  
+  useEffect(() => {
+    setData(EmployeeData);
+  }, []);
+  console.log(data);
+  return (
+    <>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Department</th>
+            <th>Salary</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            data.map((employee) => {
+              
+                <tr>
+                  <td>{employee.id}</td>
+                  <td>{employee.name}</td>
+                  <td>{employee.position}</td>
+                  <td>{employee.department}</td>
+                  <td>{employee.salary}</td>
+                </tr>
+              
+            })
+          }
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App;
---
+++ 07-a2r-states/src/EmployeeData.js
@@ -0,0 +1,31 @@
+const EmployeeData = [
+    {
+        id: 1,
+        name: "John Doe",
+        position: "Software Engineer",
+        department: "Engineering",
+        salary: 80000
+    },
+    {
+        id: 2,
+        name: "Jane Smith",
+        position: "Product Manager",
+        department: "Product",
+        salary: 90000
+    }, 
+    {
+        id: 3,
+        name: "Michael Johnson",
+        position: "UX Designer",
+        department: "Design",
+        salary: 75000
+    }, 
+    {
+        id: 4,
+        name: "Emily Davis",
+        position: "Data Analyst",
+        department: "Data Science",
+        salary: 85000
+    }
+]
+export default EmployeeData;
